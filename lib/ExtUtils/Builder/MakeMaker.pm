Leont/extutils-builder-plan

commit 6ca54e557154103d4cb7f11ae947a76422d17573

Dependency quote pure_all too

lib/ExtUtils/Builder/MakeMaker.pm
@@ -37,17 +37,18 @@ sub make_entry {
 }
 
 sub postamble {
-	my ($self, %args) = @_;
-	my @ret;
+	my ($maker, %args) = @_;
+	my (@ret, @all_deps);
 	if ($args{plans}) {
 		my @plans = ref $args{plans} eq 'ARRAY' ? @{ $args{plans} } : $args{plans};
-		push @ret, 'pure_all :: ' . join ' ', map { $_->roots } @plans;
+		push @all_deps, map { $_->roots } @plans;
 		push @ret, map { make_entry($self, $_->target, [ $_->dependencies ], [ $_ ]) } map { $_->nodes } @plans;
 	}
 	if($args{actions}) {
-		push @ret, 'pure_all :: extra_actions';
-		push @ret, make_entry($self, 'extra_actions', [], [ @{ $args{actions} } ]);
+		push @all_deps, 'extra_actions';
+		push @ret, make_entry($maker, 'extra_actions', [], [ @{ $args{actions} } ]);
 	}
+	unshift @all_deps, 'pure_all :: ' . join ' ', map { quote_dep($maker, $_) } @all_deps if @all_deps;
 	return join "\n\n", @ret;
 }
 

commit 14d36b9f1d722fac3d2b84972a574e23657adf2a

Don't forget pure_all in postamble
This was a regression in 6ca54e55

lib/ExtUtils/Builder/MakeMaker.pm
@@ -51,7 +51,7 @@ sub postamble {
 		push @all_deps, 'extra_actions';
 		push @ret, make_entry($maker, 'extra_actions', [], [ @{ $args{actions} } ]);
 	}
-	unshift @all_deps, 'pure_all :: ' . join ' ', map { quote_dep($maker, $_) } @all_deps if @all_deps;
+	unshift @ret, 'pure_all :: ' . join ' ', map { quote_dep($maker, $_) } @all_deps if @all_deps;
 	return join "\n\n", @ret;
 }
 