Leont/extutils-builder-plan

commit 784d22aeb2ba86c9efc9e70ed2e18007dc2e19ac

Greatly simplified to_code semantics

lib/ExtUtils/Builder/Action/Command.pm
@@ -16,17 +16,14 @@ sub _preference_map {
 	};
 }
 
-sub new {
-	my ($class, %args) = @_;
-	return $class->SUPER::new(%args);
-}
-
 sub to_code {
-	my $self = shift;
+	my ($self, %args) = @_;
 	require Data::Dumper;
-	my $serialized = Data::Dumper->new([ $self->to_command ])->Terse(1)->Indent(0)->Dump;
-	$serialized =~ s/ \A \[ (.*?) \] \z /$1/xms;
-	return "sub { require IPC::System::Simple; IPC::System::Simple::systemx($serialized); }";
+	my $serialized = Data::Dumper->new($self->{command})->Terse(1)->Indent(0)->Dump;
+	my $skip_loading = $args{skip_loading} || '';
+	my $loading = $skip_loading ? '' : 'require IPC::System::Simple; ';
+	my $module = $skip_loading eq 'main' ? '' : 'IPC::System::Simple::';
+	return join '', $loading, $module, 'systemx(', $serialized, ')';
 }
 
 sub to_command {

lib/ExtUtils/Builder/Action/Code.pm
@@ -5,54 +5,20 @@ use warnings;
 
 use parent 'ExtUtils::Builder::Role::Action::Code';
 
-use Carp            ();
+use Carp ();
 
 sub new {
 	my ($class, %args) = @_;
-	Carp::croak('Need to define at least one of code or serialized') if !$args{code} && !$args{serialized};
+	Carp::croak('Need to define code') if !$args{code};
+	$args{modules} ||= [];
 	my $self = $class->SUPER::new(%args);
-	$self->{code} = $args{code} if $args{code};
-	$self->{serialized} = $args{serialized} if $args{serialized};
 	return $self;
-}
-
-my %code_cache;
-sub code {
-	my $self = shift;
-	return $self->{code} ||= do {
-		my $self = shift;
-		my $code = $self->to_code(skip_loading => 1);
-		$code_cache{$code} ||= eval($code) || Carp::croak("Couldn't evaluate serialized: $@");
-	};
-}
-
-sub serialized {
-	my $self = shift;
-	return $self->{serialized} ||= do {
-		require B::Deparse;
-		my $core = B::Deparse->new('-sCi0')->coderef2text($self->{code});
-		$core =~ s/ \A { ( .* ) } \z /$1/msx;
-		$core =~ s/ \A \n? (.*?) ;? \n? \z /$1/mx;
-		$core;
-	};
 }
 
 sub to_code {
 	my ($self, %opts) = @_;
-	my @modules = $opts{skip_loading} ? () : map { "require $_; " } $self->modules;
-	my $args = $self->_get_arguments('unshift @_, %s; ');
-	return join '', 'sub { ', @modules, $args, $self->serialized, ' }';
-}
-
-sub _to_call {
-	my $self = shift;
-	my $serialized = $self->to_code(skip_loading => 1);
-	return $serialized =~ /\A sub\ \{ [ ] ([\w:]+) \( \@_ \) [ ] \} \z /x ? $1 : "$serialized->"
-}
-
-sub message {
-	my $self = shift;
-	return $self->{message};
+	my @modules = $opts{skip_loading} ? () : map { "require $_" } $self->modules;
+	return join '; ', @modules, $self->{code};
 }
 
 1;
@@ -62,11 +28,9 @@ sub message {
 =head1 SYNOPSIS
 
  my $action = ExtUtils::Builder::Action::Code->new(
-     code       => \&Frob::nicate,
-     serialized => 'Frob::nicate(@_)',
-     message    => 'frobnicateing foo',
-     arguments  => [ source => 'foo'],
+     code      => 'Frob::nicate(@_)',
      modules    => ['Frob'],
+     message   => 'frobnicateing foo',
  );
  $action->execute(target => 'bar');
  say "Executed: ", join ' ', @$_, target => 'bar' for $action->to_command;
@@ -79,9 +43,9 @@ This is a primitive action object wrapping a piece of perl code. The easiest way
 
 This is a code-ref containing the action. On execution, it is passed the arguments of the execute method; when run as command it is passed @ARGV. In either case, C<arguments> is also passed. Of not given, it is C<eval>ed from C<serialized>.
 
-=attr serialized
+=attr modules
 
-This is the stringified form of the code of the action. For execution, it's put in a sub with the action's arguments as the subs arguments. If not given, it's decompiled from C<code>.
+This is an optional list of modules that will be dynamically loaded before the action is run in any way. This attribute is optional.
 
 =method to_code
 

lib/ExtUtils/Builder/Action/Function.pm
@@ -10,46 +10,34 @@ sub new {
 	my ($class, %args) = @_;
 	croak 'Attribute module is not defined' if not defined $args{module};
 	croak 'Attribute function is not defined' if not defined $args{function};
+	$args{fullname} = join '::', $args{module}, $args{function};
 	$args{exports} ||= 0;
+	$args{message} ||= "Calling $args{fullname}";
+	$args{arguments} ||= [];
+	$args{modules} = [ $args{module} ];
 	my $self = $class->SUPER::new(%args);
 	return $self;
 }
 
-for my $attr (qw/module function exports/) {
-	my $method = sub {
-		my $self = shift;
-		return $self->{$attr};
-	};
-	no strict 'refs';
-	*{$attr} = $method;
-}
+sub execute {
+	my ($self, %args) = @_;
+	(my $filename = "$self->{module}.pm") =~ s{::}{/}g;
+	require $filename;
 
-sub fullname {
-	my $self = shift;
-	return join '::', $self->module, $self->function;
-}
-
-sub message {
-	my $self = shift;
-	return $self->{message} ||= 'Calling ' . $self->fullname;
-}
-
-sub code {
-	my $self = shift;
-	no strict 'refs';
-	return \&{ $self->fullname };
-}
-
-sub _to_call {
-	my $self = shift;
-	return $self->exports ? $self->function : $self->fullname;
+	my $code = do { no strict 'refs'; \&{ $self->{fullname} } };
+	$code->(@{ $self->{arguments} });
 }
 
 sub to_code {
-	my $self = shift;
-	my ($module, $fullname) = ($self->module, $self->fullname);
-	my $args =  $self->_get_arguments(' unshift @_, %s;');
-	return "sub { require $module;$args $fullname(\@_) }";
+	my ($self, %args) = @_;
+	my $skip_loading = $args{skip_loading} || '';
+	my $shortcut = $args{skip_loading} && $args{skip_loading} eq 'main' && $self->{exports};
+	my $name = $shortcut ? $self->{function} : $self->{fullname};
+	my @modules = $opts{skip_loading} ? () : map { "require $_" } $self->modules;
+	my $arguments = %{ $self->{arguments} } ? do {
+		require Data::Dumper; (Data::Dumper->new([ $args{arguments} ])->Terse(1)->Indent(0)->Dump =~ /^ \[ (.*) \] $/x)[0]
+	} : '';
+	return join '; ', @modules, sprintf '%s(%s)', $name, $arguments;
 }
 
 1;
@@ -61,8 +49,9 @@ sub to_code {
  my $action = ExtUtils::Builder::Action::Function->new(
      module    => 'Frob',
      function  => 'nicate',
+     arguments => { target => 'bar' },
  );
- $action->execute(target => 'bar');
+ $action->execute();
  say "Executed: ", join ' ', @$_, target => 'bar' for $action->to_command;
 
 =head1 DESCRIPTION
@@ -70,13 +59,16 @@ sub to_code {
 This Action class is a specialization of L<Action::Code|ExtUtils::Builder::Action::Code> that makes the common case of calling a simple function easier. The first statement in the synopsis is roughly equivalent to:
 
  my $action = ExtUtils::Builder::Action::Code->new(
-     code       => \&Frob::nicate,
-     serialized => 'Frob::nicate(@_)',
-     message    => 'Calling Frob::nicate',
+     code       => 'Frob::nicate(target => 'bar')',
      modules    => ['Frob'],
+     message    => 'Calling Frob::nicate',
  );
 
 Except that is serializes more cleanly.
+
+=attr arguments
+
+These are additional arguments to the action, that are passed on regardless of how the action is run. This attribute is optional.
 
 =attr module
 

lib/ExtUtils/Builder/Overview.pod
@@ -23,7 +23,7 @@ An action can be consumed in many ways.
 
 =item * execute(%args)
 
-This is often the simplest way of dealing with an action. It simple performs the action immediately, and will throw an exception on failure. The commands may take arbitrary named arguments.
+This is often the simplest way of dealing with an action. It simple performs the action immediately, and will throw an exception on failure.
 
 =item * to_command(%opts)
 
@@ -33,13 +33,11 @@ This converts the action into a list of commands to be executed. The elements of
    system(@$command);
  }
 
-It can take two optional named arguments: C<perl> for the path to perl, and C<config> for an L<ExtUtils::Config> object used the find the current perl. The command may take additional named arguments as pairs on the command line.
+It can take two optional named arguments: C<perl> for the path to perl, and C<config> for an L<ExtUtils::Config> object used the find the current perl.
 
 =item * to_code()
 
-This converts the action into a list of strings to be C<eval>ed. The evaled values are coderefs that take arguments the same way C<execute> does.
-
-This can be useful when you want to serialize the work that is to be done but don't want to force it to shell out.
+This converts the action into a list of strings to be C<eval>ed in order to execute them. This can be useful when you want to serialize the work that is to be done but don't want to force it to shell out.
 
 =item * flatten()
 
@@ -51,7 +49,7 @@ On L<composite|ExtUtils::Builder::Role::Action::Composite> actions, C<flatten> c
 If a consumer can consume actions in more than one way, the C<preference> method can be used to choose between options. This function expects a list of options out of C<code>, C<command>, C<execute> and C<flatten>. You probably want to flatten your action first, as different constituents may have different preferences.
 
  for my $action ($work->flatten) {
-   my $preference = $self->preference(qw/code command/);
+   my $preference = $self->preference('code', 'command');
    push @serialized, ($preference eq 'code')
      ? [ eval => $action->to_code ] 
      : [ exec => $action->to_command ];

lib/ExtUtils/Builder/Role/Action/Code.pm
@@ -7,13 +7,6 @@ use parent 'ExtUtils::Builder::Role::Action::Primitive';
 
 use Config;
 
-sub new { 
-	my ($class, %args) = @_;
-	$args{modules} ||= [];
-	$args{arguments} ||= {};
-	return $class->SUPER::new(%args);
-}
-
 sub _preference_map {
 	return {
 		execute => 3,
@@ -23,27 +16,16 @@ sub _preference_map {
 	};
 }
 
-sub execute {
-	my ($self, %opts) = @_;
-	for my $module ($self->modules) {
-		(my $filename = "$module.pm") =~ s{::}{/}g;
-		require $filename;
-	}
-	$opts{logger}->($self->message) if $opts{logger} && !$opts{quiet} && exists $self->{message};
-	$self->code->(%{ $self->{arguments} }, %opts);
-	return;
-}
-
-sub _get_arguments {
-	my ($self, $format, $default) = @_;
-	return $default || '' if !%{ $self->{arguments} };
-	require Data::Dumper;
-	return sprintf $format, (Data::Dumper->new([ $self->{arguments} ])->Terse(1)->Indent(0)->Dump =~ /^ \{ (.*) \} $/x)[0];
-}
-
 sub modules {
 	my $self = shift;
 	return @{ $self->{modules} };
+}
+
+sub execute {
+	my ($self, %opts) = @_;
+	$opts{logger}->($self->{message}) if $opts{logger} && !$opts{quiet} && exists $self->{message};
+	eval $self->to_code() . '; 1' or die $@;
+	return;
 }
 
 sub _get_perl {
@@ -60,11 +42,16 @@ sub _get_perl {
 	}
 }
 
+sub to_code {
+	my ($self, %opts) = @_;
+	my @modules = $opts{skip_loading} ? () : map { "require $_; " } $self->modules;
+	return join '', @modules, $self->code(%opts);
+}
+
 sub to_command {
 	my ($self, %opts) = @_;
 	my @modules = map { "-M$_" } $self->modules;
-	my $args = $self->_get_arguments('%s, @ARGV', '@ARGV');
-	return [ _get_perl(%opts), @modules, '-e', $self->_to_call() . "($args)" ];
+	return [ _get_perl(%opts), @modules, '-e', $self->to_code(skip_loading => 'main') ];
 }
 
 1;
@@ -74,14 +61,6 @@ sub to_command {
 =head1 DESCRIPTION
 
 This role provides most functionality of Code Actions.
-
-=attr arguments
-
-These are additional arguments to the action, that are passed on regardless of how the action is run. This attribute is optional.
-
-=attr modules
-
-This is an optional list of modules that will be dynamically loaded before the action is run in any way. This attribute is optional.
 
 =attr message
 
@@ -98,7 +77,5 @@ This returns an arrayref containing a command for this action.
 =method preference
 
 This will prefer handling methods in the following order: execute, code, command, flatten
-
-=method code
 
 …

t/10-action-code.t
@@ -14,34 +14,35 @@ use Test::LivesOK 'lives_ok';
 	my $action;
 	our $callback = sub {};
 	my %args = (
-		code => sub { $callback->(@_) },
-		serialized => '$callback->(@_)',
+		code    => '$::callback->()',
 		message => 'callback',
 	);
 	lives_ok { $action = ExtUtils::Builder::Action::Code->new(%args) } 'Can create new object';
 
 	{
 		my @actions;
-		local $callback = sub { push @actions, @_ };
+		local $callback = sub { push @actions, 1 };
 
 		lives_ok { $action->execute(quiet => 1) } 'Can execute command';
+		is_deeply(\@actions, [1], 'Command is executed quietly');
 	}
 
 	{
 		my (@actions, @messages);
-		local $callback = sub { push @actions, @_ };
+		local $callback = sub { push @actions, 2 };
 
 		lives_ok { $action->execute(logger => sub { push @messages, @_ }) } 'Can execute command';
 
+		is_deeply(\@actions, [2], 'Command is executed with logging');
 		is_deeply(\@messages, [ 'callback' ], 'Got the message');
 	}
 
 	my @serialized = $action->to_command;
 	is(scalar(@serialized), 1, 'Got one command');
-	my ($command, @arguments) = @{ +shift @serialized };
+	my ($command, @arguments) = @{ shift @serialized };
 	is($command, $Config{perlpath}, "Command is $Config{perlpath}");
-	is_deeply(\@arguments, [ '-e', "sub { $args{serialized} }->(\@ARGV)" ], '');
-	is($action->to_code, "sub { $args{serialized} }", 'to_code is "sub { $input }"');
+	is_deeply(\@arguments, [ '-e', $args{code} ], 'to_command gives correct arguments');
+	is($action->to_code, $args{code}, 'to_code is "$input"');
 
 	is($action->preference, 'execute', 'Prefered means is "execute"');
 	is($action->preference(qw/code command/), 'code', 'Prefered means between "code" and "command" is "code"');

t/20-node.t
@@ -10,8 +10,9 @@ use Test::LivesOK 'lives_ok';
 use ExtUtils::Builder::Node;
 use ExtUtils::Builder::Action::Code;
 
-my ($node, @triggered);
-my @actions = map { my $num = $_; ExtUtils::Builder::Action::Code->new(code => sub { push @triggered, $num }) } 1, 2;
+our @triggered;
+my @actions = map { ExtUtils::Builder::Action::Code->new(code => "push \@::triggered, $_") } 1, 2;
+my $node;
 lives_ok { $node = ExtUtils::Builder::Node->new(target => 'foo', dependencies => [ qw/bar baz/ ], actions => \@actions) } 'Can create new object';
 
 lives_ok { $node->execute } 'Can execute quiet command';

t/30-plan.t
@@ -11,13 +11,20 @@ use ExtUtils::Builder::Node;
 use ExtUtils::Builder::Plan;
 use ExtUtils::Builder::Action::Code;
 
-my @triggered;
-my @nodes = map { my $num = $_; ExtUtils::Builder::Node->new(target => "foo$_", dependencies => [], actions => [ ExtUtils::Builder::Action::Code->new(code => sub { push @triggered, $num }) ]) } 0 .. 2;
+our @triggered;
+my @nodes = map {
+	ExtUtils::Builder::Node->new(
+		target => "foo$_",
+		dependencies => [],
+		actions => [ ExtUtils::Builder::Action::Code->new(code => "push \@::triggered, $_" ) ]
+	)
+} 0 .. 2;
+
 my $root = ExtUtils::Builder::Node->new(target => "foo", dependencies => [ map { "foo$_" } 0..2 ], actions => []);
 my $plan;
 lives_ok { $plan = ExtUtils::Builder::Plan->new(nodes => [ @nodes, $root ], roots => 'foo') } 'Plan could be created';
 
-lives_ok { $plan->execute } 'executing gave no errors';
+lives_ok { $plan->execute } 'Executing gave no errors';
 
 is_deeply(\@triggered, [ 0..2 ], 'All actions triggered');
 

commit 0003153d1859cd8c2b66c71cc2b1df47acac1ac9

Fix Command::to_code serialization
This was broken in 784d22ae

lib/ExtUtils/Builder/Action/Command.pm
@@ -19,7 +19,8 @@ sub _preference_map {
 sub to_code {
 	my ($self, %args) = @_;
 	require Data::Dumper;
-	my $serialized = Data::Dumper->new($self->{command})->Terse(1)->Indent(0)->Dump;
+	my $serialized = Data::Dumper->new([$self->{command}])->Terse(1)->Indent(0)->Dump;
+	$serialized =~ s/ \A \[ (.*?) \] \z /$1/xms;
 	my $skip_loading = $args{skip_loading} || '';
 	my $loading = $skip_loading ? '' : 'require IPC::System::Simple; ';
 	my $module = $skip_loading eq 'main' ? '' : 'IPC::System::Simple::';

t/10-action-command.t
@@ -14,7 +14,7 @@ lives_ok { $action = ExtUtils::Builder::Action::Command->new(command => [ $^X, '
 
 is_deeply($action->to_command, [$^X, '-e0'], 'Returns perl -e0');
 
-like($action->to_code, qr/ \Q$^X\E .+? -e0 /x, 'to_code returns something that might be sensible');
+like($action->to_code, qr/ systemx \( '\Q$^X\E','-e0' \) /x, 'to_code returns something that might be sensible');
 
 lives_ok { $action->execute(quiet => 1) } 'Can execute quiet command';
 