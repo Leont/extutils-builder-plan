92f0a32a61861243021b3bd2b96bfdcaeffadb28
Leon Timmermans <fawaka@gmail.com>
2024-03-30 04:18:13 +0100

    Add add_helper method to Planner::Extension

--- lib/ExtUtils/Builder/Planner/Extension.pm
+++ lib/ExtUtils/Builder/Planner/Extension.pm
@@ -15,10 +15,23 @@ sub add_delegate {
 	return;
 }
 
+sub add_helper {
+	my ($self, $planner, $as, $helper) = @_;
+	$planner->add_delegate($as, sub {
+		my ($self, @args) = @_;
+		return $helper->(@args);
+	});
+	return;
+}
+
 1;
 
 #ABSTRACT: a base class for Planner extensions
 
 =method add_delegate($planner, $as, $make_node)
 
-This adds a delegate function to C<$planner> with name C<$name>. The function must return zero or more L<node|ExtUtils::Builder::Node> objects that will be added to the plan.
+This adds a delegate function to C<$planner> with name C<$as>. The function must return zero or more L<node|ExtUtils::Builder::Node> objects that will be added to the plan.
+
+=method add_helper($helper, $as, $sub)
+
+This adds a helper function to C<$planner> with name <$as>. It will return whatever C<$sub> returns, but will not affect the build tree in any way.
